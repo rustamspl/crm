fd1a8b04944a7c846d3ec30f674ecb5d7bbce61a
yeldar@bk.ru <yeldar@bk.ru>
2016-02-17 22:50:41 +0600

    commit

--- web.go
+++ web.go
@@ -19,7 +19,7 @@ func init() {
 	log.Println(err)
 	log.Println("openshift_port="+os.Getenv("OPENSHIFT_GO_PORT"))
 
-	err = orm.RegisterDataBase("default", "mysql", os.Getenv("OPENSHIFT_MYSQL_DB_USERNAME")+":"+os.Getenv("OPENSHIFT_MYSQL_DB_PASSWORD")+"@tcp("+os.Getenv("OPENSHIFT_MYSQL_DB_HOST")+":"+os.Getenv("OPENSHIFT_MYSQL_DB_PORT")+")/golang?charset=utf8")
+	err = orm.RegisterDataBase("default", "mysql", os.Getenv("OPENSHIFT_MYSQL_DB_USERNAME")+":"+os.Getenv("OPENSHIFT_MYSQL_DB_PASSWORD")+"@tcp("+os.Getenv("OPENSHIFT_MYSQL_DB_HOST")+":"+os.Getenv("OPENSHIFT_MYSQL_DB_PORT")+")/"+os.Getenv("OPENSHIFT_APP_NAME")+"?charset=utf8")
 	if err!=nil{
 		panic(err)
 	}else{
